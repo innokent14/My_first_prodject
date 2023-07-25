734806df8962e5400ea6e6afe377c65e9b85d705
innokent14 <innokent14@mail.ru>
2023-07-25 19:11:25 +0300

    models and migrate

--- advertisements/add_main/models.py
+++ advertisements/add_main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisements(models.Model):
+    title = models.CharField('Title', max_length=128)
+    description = models.TextField('Description')
+    price = models.DecimalField('Price', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Auction', help_text='торг')
+    created_ad = models.DateTimeField(auto_now_add=True)
+    update_ad = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'id: {self.id}, title: {self.title},  price: {self.price}'
+
+    class Meta:
+        db_table = "advertisements"
--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    '2': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
